c3d01edc15cad0b5d8751f54338cc1448aa221a0
Rostyslav Litvinov <rostislav.litvinov@ekreative.com>
2021-11-05 23:35:00 +0200

    Initial Commit

---
+++ GyroPlay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GyroPlay
+//
+//  Created by Rostyslav Litvinov on 05.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GyroPlay/GyroPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  GyroPlayApp.swift
+//  GyroPlay
+//
+//  Created by Rostyslav Litvinov on 05.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct GyroPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
